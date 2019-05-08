ed5af38c4e23466a0fd45a56cc1aff17cb27c445
juarezfdo <juarezfdo96@gmail.com>
2019-05-08 15:05:24 -0500

    Practica2

--- imc/index.php
+++ imc/index.php
@@ -3,24 +3,40 @@
 	include "persona.php";
 	//Instanciar la clase
 	$persona = new persona();
-	//Asignar valores a los propiedades del objeto
-	$persona->setEdad(30);
-	$persona->setAltura(1.80);
-	$persona->setPeso(80);
-
-	//Impresiones de los resultados
-	echo "<br>Edad: ".$persona->edad;
-	echo "<br>Altura: ".$persona->altura;
-	echo "<br>Peso: ".$persona->peso;
-	echo "<br>IMC: ".$persona->imc();
  ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 	<title>Calculadora IMC</title>
 </head>
 <body>
-
+	<div class="container">
+		<br>
+		<div class="row">
+			<div class="col-6">
+				<h3 align="center">Calculadora de IMC</h3>
+				<?php $persona->crearFormulario(); ?>
+				<?php 
+				if (isset($_POST["calcular"])) {
+					$persona->insertar();
+				}
+				 ?>
+				 <br>
+			</div>
+			<div class="col-6">
+				<h3 align="center">Resultados obtenidos</h3>
+				<?php $persona->mostrar(); ?>
+			</div>		
+		</div>
+	</div>
+	
 </body>
 </html>
--- imc/persona.php
+++ imc/persona.php
@@ -40,5 +40,89 @@
 		public function imc2(){
 			return $this->getPeso()/($this->getAltura() * $this->getAltura());
 		}
+
+		public function insertar(){
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "taw";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			} 
+
+			if (isset($_POST["calcular"])) {
+				$Edad = $_POST['Edad'];
+				$peso = $_POST["peso"];
+				$altura = $_POST["altura"];
+				$imc = $peso/($altura*$altura);
+				?> <br><h4>Tu IMC es de: </h4> <?php echo $imc;
+			}
+			$sql = "INSERT INTO practica2_imc (Edad,peso,altura,imc) VALUES ('{$Edad}','{$peso}','{$altura}','{$imc}'); ";
+			if($conn->query($sql) === TRUE){
+				
+			}else{
+				 echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+		}
+
+		public function crearFormulario(){
+			?>
+			<form method="POST">
+				<div class="form-group">
+					<div class="row">
+						<div class="col-6">
+							<label for="Edad">Edad</label>
+							<input type="number" name="Edad" id="Edad" class="form-control"required>
+						</div>
+					</div>
+					
+				</div>
+				<div class="form-group">
+					<div class="row">
+						<div class="col-6">
+							<label for="peso">Peso:</label>
+							<input class="form-control" type="text" name="peso" id="peso"required>
+						</div>
+						<div class="col-6">
+							<label for="altura">Altura</label>
+							<input class="form-control" type="text" name="altura" id="altura"required>						
+						</div>
+					</div>
+				</div>
+				<button type="submit" class="btn btn-primary btn-block" name="calcular">Calcular</button>
+			</form>
+			<?php 
+		}
+		function mostrar(){
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "taw";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			} 
+
+			$sql = "SELECT edad,peso,altura,imc FROM practica2_imc;";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+			    // output data of each row
+			    while($row = $result->fetch_assoc()) {
+			        echo "Edad: ".$row["edad"]. ", Peso:" . $row["peso"].", Altura: ". $row["altura"].", IMC:  ".$row["imc"]. "<br>";
+			    }
+			} else {
+			    echo "0 resultados";
+			}
+		}
 	}
  ?>
